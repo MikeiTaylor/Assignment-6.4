420d15726d4d21f544473ecde963463322ad4ee2
MikeiTaylor <143642773+MikeiTaylor@users.noreply.github.com>
2024-10-04 21:08:59 -0500

    Arrays & Objects

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+const myLuck=["Good things are coming you'r way", "Better luck next time", "Not so lucky", "Maybe next time", "Beginnerss Luck"];
+console.log(myLuck[1]);
+
+myLuck.push("Testing your Luck?");
+
+
+const user = {
+name: "Michael",
+age: 23,
+married: false,
+purchases: ["Tablet","Car","Phone","Laptop"],
+sayAge: function() {
+console.log(this.age);
+} 
+};
